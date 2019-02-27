fe069712f8ddba94ac285927e23b9d77dfb0453c
yuzexia <327932801@qq.com>
2019-02-27 12:34:36 +0800

    add create _html,add copy handle

--- js/main.js
+++ js/main.js
@@ -1,16 +1,58 @@
 $(document).ready(function() {
-
-
+    var uid = 47491;
+    // 统计数据元素
+    var numberBox = $('.text i');
+    // code元素
+    var codeBox = $('.num');
+    var codeInputBox = $('#inviteCode');
+    // 邀请链接元素
+    var invitUrlBox = $('#inviteUrl');
+    var codeCopy = $('#codeCopy');
+    var urlCopy = $('#urlCopy');
     // 获取初始化信息
     $.ajax({
-        url: 'http://xclub.pre.transsion.net/api/mobile/index.php?version=9&module=recommend_user_statistics&uid=1',
+        url: 'http://xclub.pre.transsion.net/api/mobile/index.php?version=9&module=recommend_user_statistics&uid='+ uid,
         type: 'get',
         dataType: 'json',
         success: function(res) {
             console.log(res);
+            if (Number(res.code) === 0) {
+                var data = res.data
+                var code = data.invitation_code
+                var _arr = [data.month_xgolds, data.all_xgolds, data.month_count, data.all_count]
+                $.each(numberBox, function(index, item) {
+                    $(item).text(_arr[index])
+                })
+                $.each(codeBox, function(index, item) {
+                    $(item).text(code[index])
+                })
+                codeInputBox.val(code);
+                invitUrlBox.val('http://www.infinix.com/code=' + code);
+                codeCopy.attr('data-clipboard-text', code)
+            } else {
+                alert('error:::出错了。。。')
+            }
         },
         error: function(err) {
             console.log(err);
         }
+    })
+
+    // 复制操作
+    // 复制code
+    var codeClipboard = new ClipboardJS('#codeCopy');
+    codeClipboard.on('success', function() {
+        console.log('复制成功...');
+    })
+    codeClipboard.on('error', function() {
+        console.log('复制失败...');
+    })
+    // 复制url
+    var urlClipboard = new ClipboardJS('#urlCopy');
+    urlClipboard.on('success', function() {
+        console.log('复制成功...');
+    })
+    urlClipboard.on('error', function() {
+        console.log('复制失败...');
     })
 })
--- js/record.js
+++ js/record.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    var startX, startY, dir;
+    var startX, startY, dir, maxPage;
     var ajaxStatus = true;
     var loadEle = $('#loading');
+    var noMoreEle = $('.no-more')
+    var boxEle = $('#recordBox')
+    var page = 1;
+    var limit = 20;
+    var uid = 47491;
+    var recordData = [];
+    loadData(page);
     document.addEventListener('touchstart', function(e) {
         var touch = e.touches[0]
         startX = touch.pageX;
@@ -36,16 +43,21 @@ $(document).ready(function() {
         var scrollTop = $this.scrollTop();
         var viewH = $this.innerHeight();
         var scrollHeight = $(document).height();
-        console.log(scrollTop,  viewH, scrollHeight)
+        // console.log(scrollTop,  viewH, scrollHeight)
         if (scrollTop + viewH >= scrollHeight && ajaxStatus){
             console.log('应该加载了...');
-            loadData();
+            if (++page <= maxPage) {
+                loadData(page);
+            } else {
+                maxPage > 1 ? hideMore(noMoreEle, boxEle, 'padding-bottom') : '';
+            }
         }
     })
     
-    function loadData() {
+    // 获取邀请记录
+    function loadData(page) {
         $.ajax({
-            url: 'http://www.baidu.com',
+            url: 'http://xclub.pre.transsion.net/api/mobile/index.php?version=9&module=recommend_user_list&limit='+ limit +'&uid='+ uid +'&page='+ page,
             type: 'get',
             dataType: 'json',
             beforeSend: function() {
@@ -56,6 +68,16 @@ $(document).ready(function() {
                 console.log(res);
                 loadEle.hide();
                 ajaxStatus = true;
+                if (Number(res.code) === 0) {
+                    var _data = res.data
+                    maxPage = Math.ceil(_data.count / 20)
+                    if (_data.list.length) {
+                        recordData =  _data.list
+                        createHtml(recordData);
+                    }
+                } else {
+                    alert('error:出错了。。。')
+                }
             },
             error: function(err) {
                 console.log(err);
@@ -63,5 +85,41 @@ $(document).ready(function() {
                 ajaxStatus = true;
             }
         })
+    }
+
+    // 隐藏更多
+    function hideMore(ele, box, classname) {
+        $(ele).show();
+        $(box).addClass(classname);
+        setTimeout(function(){
+            ele.hide();
+            box.removeClass(classname);
+        }, 2000);
+    }
+
+    // 生成html
+    function createHtml(data) {
+        var contentBox = $('#recordContent')
+        var _html = ''
+        $.each(data, function(index, item) {
+            _html += '<div class="record-row">' +
+                        '<span>'+ item.username +'</span>' +
+                        '<span>'+ format(item.create_time) +'</span>' +
+                        '<span>'+ item.xgolds +' XGold</span>' +
+                    '</div>'
+        })
+        $(_html).appendTo(contentBox);
+    }
+
+    // 格式化时间
+    function format(date) {
+        var d = new Date(date * 1000) || new Date();
+        var fullYear = d.getFullYear();
+        var month = d.getMonth() + 1 >= 10 ? d.getMonth() + 1 : '0' + Number(d.getMonth() + 1);
+        var date = d.getDate() >= 10 ? d.getDate() : '0' + d.getDate();
+        var hours = d.getHours() >= 10 ? d.getHours() : '0' + d.getHours();
+        var minutes = d.getMinutes() >= 10 ? d.getMinutes() : '0' + d.getMinutes();
+        var seconds = d.getSeconds() >= 10 ? d.getSeconds() : '0' + d.getSeconds();
+        return fullYear + '-' + month + '-' + date;
     }
 })
